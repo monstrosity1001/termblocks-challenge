13220bb6f72d87163e4a4f841ce4d56665d32776
monstrosity1001 <111359902+monstrosity1001@users.noreply.github.com>
2025-04-20 01:03:17 -0700

    Added the zustand library

---
+++ termblocks-checklist-frontend/src/store/checklistStore.ts
@@ -0,0 +1,35 @@
+import create from 'zustand';
+
+export interface ChecklistItem {
+  id: number;
+  name: string;
+  uploads: Array<any>; // To be refined
+}
+
+export interface ChecklistCategory {
+  id: number;
+  name: string;
+  items: ChecklistItem[];
+}
+
+export interface Checklist {
+  id: number;
+  title: string;
+  description: string;
+  public_id: string;
+  categories: ChecklistCategory[];
+}
+
+interface ChecklistStore {
+  checklists: Checklist[];
+  setChecklists: (checklists: Checklist[]) => void;
+  selectedChecklist: Checklist | null;
+  setSelectedChecklist: (checklist: Checklist | null) => void;
+}
+
+export const useChecklistStore = create<ChecklistStore>((set) => ({
+  checklists: [],
+  setChecklists: (checklists) => set({ checklists }),
+  selectedChecklist: null,
+  setSelectedChecklist: (checklist) => set({ selectedChecklist: checklist }),
+}));
